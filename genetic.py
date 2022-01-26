ba9494252c475a6ae286c0a9b69a8fbe544c9cf5
LeonardoESousa <leonardo.sousa137@gmail.com>
2022-01-26 21:31:23 +0100

    updates

---
+++ genetic.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+
+
+
+## Orders the Report
+# args: None
+# i)  Try: Incluir elite no report. 
+# ii) Pegar os dados do Report. Usar np.loadtxt. Ordenar (maior pro menor se maximize=True, else menor pro maior)
+# iii)Retorna matriz ordenada. 
+
+
+
+## TNG
+# args: matriz ordenada
+# i)   Selecionar conjuntos de pais (numero de pais variavel)
+# ii)  Fazer crossover e mutacao (mutacao variavel)
+# iii) Escrever filhos em um novo arquivo  (id, genes)
+
+
+
+
