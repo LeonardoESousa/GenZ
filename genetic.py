7023a614ed56d6ae7a26558839caf5cad2572756
laurasimonassi <laurasimonassirp@gmail.com>
2022-01-29 16:20:33 -0300

    Orders population array

--- genetic.py
+++ genetic.py
@@ -8,8 +8,24 @@ import os
 # i)  Try: Incluir elite no report. 
 # ii) Pegar os dados do Report. Usar np.loadtxt. Ordenar (maior pro menor se maximize=True, else menor pro maior)
 # iii)Retorna matriz ordenada. 
+maximize=True
+def order():
+    r=np.loadtxt('Report.txt')
+   
+    try:
+        e=np.loadtxt('Elite.txt')
+        f=np.vstack((r,e))
+    except:
+        f=r
 
+    arr=np.array(f)
 
+    if maximize==True:
+        sorted_arr=sorted(arr,reverse=True,key=lambda x:x[-1])
+        #sorted_arr=np.sort(arr,axis=-1)
+    else:
+        sorted_arr=sorted(arr,reverse=False,key=lambda x:x[-1])
+    return(sorted_arr)
 
 ## TNG
 # args: matriz ordenada
