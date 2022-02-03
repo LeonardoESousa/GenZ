4d95eb293c0332a1a9f32e59ebb2233496ed40b9
LeonardoESousa <leonardo.sousa137@gmail.com>
2022-02-03 16:16:16 +0100

    novas funcoes a fazer

--- genetic.py
+++ genetic.py
@@ -3,12 +3,19 @@ import os
 
 
 np.set_printoptions(suppress=True)
-## Orders the Report
+## max_id (Pedao)
+# args: None
+# i) Abre o Report.dat com o np.loadtxt e pega o maior numero da primeira coluna.
+# ii) Retorna esse maior numero 
+
+
+
+## Orders the Report (Laura)
 # args: None
 # i)  Try: Incluir elite no report. 
 # ii) Pegar os dados do Report. Usar np.loadtxt. Ordenar (maior pro menor se maximize=True, else menor pro maior)
 # iii)Retorna matriz ordenada. 
-
+# iv) Atualiza o arquivo Elite.dat com a nova elite
 def order(maximize=False):
     data = np.loadtxt('Report.dat')
     try:
@@ -25,11 +32,40 @@ def order(maximize=False):
 
 print(order())
 
-## TNG
-# args: matriz ordenada
-# i)   Selecionar conjuntos de pais (numero de pais variavel)
-# ii)  Fazer crossover e mutacao (mutacao variavel)
-# iii) Escrever filhos em um novo arquivo  (id, genes)
+## Best (Pedao)
+# args: matriz ordenada que sai da funcao order (numpy array).
+# i) Pega a matriz ordenada e retira apenas as linhas e colunas correspondentes ao cara de fitness mais alto. Pode haver mais de um com mesmo fitness!
+# ii) Usando um with open('Best.dat', 'w') as file:, escreve os genes e o fitness dos melhores caras até o momento em um arquivo chamado Best.dat. Usar o np.savetxt.
+# ii) No mesmo arquivo, escrever a média e o desvio padrão dos genes do melhor cara. Usar o np.mean e np.std que já calcula o de todas as colunas de uma vez.
+# Usar o np.savetxt pra escrever no mesmo arquivo.
+
+
+## TNG (Laura)
+# args: matriz ordenada (numpy array), numero de filhos (int), numero de pais (int) (pode ser 2 ou mais), maximize (Boolean)
+# i)   Chamar a funcao max_id, guardar o que ela retorna e somar um. Esse vai ser o id do primeiro filho a ser gerado, 
+# ii)  Criar um loop for para realizar o procedimento para cada filho que precisa ser gerado.
+#   iii)  Selecionar conjuntos de pais. Chance de ser escolhido é proporcional ao fitness se maximize=True, caso contrário deve ser proporcional ao inverso do fitness.
+#   iv)   Escrever um array (numero de pais x numero de genes) com os genes dos pais. Vai servir de input para a funcao de crossover.
+#   v)    Chamar a funcao de crossover e pegar o que ela retorna
+#   vi)   Chamar a funcao de mutacao e pegar o que ela retorna
+#   vii)  Usar o np.vstack para criar um array em que cada linha corresponde a um filho e cada coluna os seus genes. A primeira coluna tem de ser o numero identificador do individuo.
+#   viii) Somar um ao identficador.
+#ix)  Escrever filhos em um novo arquivo  (id, genes)
+
+
+## Crossover (Pedao)
+# args: array com genes de todos os pais
+# i)  Fazer o crossover. Crossover tem de depender de qual o tipo de gene (binario, int, float, quantas casa decimais, string)
+# ii) Funcao retorna um array com os genes do filho. 
+
+
+## Mutacao (Laura)
+# args: array com os genes de um individuo, parametro que controla a chance de mutacao. 
+# i) Fazer mutacao (mutacao variavel. inversamente proporcional ao desvio padrao dos fitness que estao na matriz argumento da funcao)
+# ii) Funcao retorna um array com os genes do filho. 
+
+
+
 
 
 
