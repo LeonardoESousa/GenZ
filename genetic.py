5affc0be3f1cc3631a44d6ad986b729f2734d01c
LeonardoESousa <leonardo.sousa137@gmail.com>
2022-02-04 23:01:59 +0100

    fixed tng function

--- genetic.py
+++ genetic.py
@@ -19,11 +19,11 @@ class Genes():
     def add_gene(self,**kwargs):
         num = len(self.genes)
         self.genes.append(num)
-        self.types[num] = kwargs['type']
+        self.types[num]  = kwargs['type']
         self.limits[num] = kwargs['space']
         if kwargs['type'] == float:
             self.formats[num] = kwargs['format']
-            self.fmts.append('%.{}f'.format(kwargs['format']))
+            self.fmts.append('%+.{}f'.format(kwargs['format']))
         else:
             self.fmts.append('%d')    
 
@@ -49,17 +49,15 @@ class Genes():
                     linha[0,g+1] = np.round(np.random.uniform(self.limits[g][0],self.limits[g][-1]),self.formats[g])
             first = np.vstack((first,linha))
         first = first[1:,:]
-        np.savetxt('NextGen.dat', first, fmt=self.fmts)
+        np.savetxt('NextGen.dat', first, fmt=self.fmts, delimiter='\t')
 
 
 ## Cria instancia da classe Genes com populacao de 10 individuos e 10 geracoes
 genes = Genes(population=10,generations=10, maximize=True)
 ## Adiciona um gene de tipo float, com valores de 0 a 100 e duas casas decimais
-genes.add_gene(type=float, space=[0,100], format=2)
-## Adiciona um gene de tipo int, com valores possíveis 0 ou 1
-genes.add_gene(type=int, space=[0,1])
-## Adiciona um gene de tipo str, com valores possíveis a, b ou c
-#genes.add_gene(type=str, space=['a','b','c'])
+genes.add_gene(type=float, space=[-5,5], format=3)
+genes.add_gene(type=float, space=[-5,5], format=3)
+genes.add_gene(type=float, space=[-5,5], format=3)
 # Printa lista que identifica cada gene
 print(genes.genes)
 # Realiza a mutacao no gene 0 que tem valor 1 agora e retorna o novo valor
@@ -71,9 +69,11 @@ genes.first_gen()
 
 ## max_id (Pedao)
 # args: None
-# i) Abre o Report.dat com o np.loadtxt e pega o maior numero da primeira coluna.
-# ii) Retorna esse maior numero 
-
+# i) Abre o NextGen.dat com o np.loadtxt e pega o maior numero da primeira coluna.
+# ii) Retorna esse numero.
+#Funcao provisoria abaixo
+def max_id():
+    return 4
 
 
 ## Orders the Report (Laura)
@@ -81,7 +81,7 @@ genes.first_gen()
 # i)  Try: Incluir elite no report. 
 # ii) Pegar os dados do Report. Usar np.loadtxt. Ordenar (maior pro menor se maximize=True, else menor pro maior)
 # iii)Retorna matriz ordenada. 
-def order(maximize=False):
+def order():
     data = np.loadtxt('Report.dat')
     try:
         elite = np.loadtxt('Elite.dat')
@@ -89,7 +89,7 @@ def order(maximize=False):
     except:
         pass
     indice = np.argsort(data[:,-1])
-    if maximize == True:
+    if genes.maximize:
         sorted_arr = data[np.flip(indice),:]
     else:
         sorted_arr = data[indice,:]
@@ -100,7 +100,7 @@ def order(maximize=False):
 # i) Pega as primeiras N linhas da matriz ordenada, onde N é o numero de elementos na elite.
 # ii) Escreve a matriz no arquivo Elite.dat 
 def elite(num_elite,sorted_arr):
-    np.savetxt('Elite.dat',sorted_arr[0:num_elite,:], delimiter='    ',fmt='%7.4f')
+    np.savetxt('Elite.dat',sorted_arr[0:num_elite,:], delimiter='\t',fmt=genes.fmts+['%.3f'])
 
 ## Best (Pedao)
 # args: matriz ordenada que sai da funcao order (numpy array).
@@ -108,36 +108,6 @@ def elite(num_elite,sorted_arr):
 # ii) Usando um with open('Best.dat', 'w') as file:, escreve os genes e o fitness dos melhores caras até o momento em um arquivo chamado Best.dat. Usar o np.savetxt.
 # ii) No mesmo arquivo, escrever a média e o desvio padrão dos genes do melhor cara. Usar o np.mean e np.std que já calcula o de todas as colunas de uma vez.
 # Usar o np.savetxt pra escrever no mesmo arquivo.
-
-
-## TNG (Laura)
-# args: matriz ordenada (numpy array), numero de filhos (int), numero de pais (int) (pode ser 2 ou mais), maximize (Boolean)
-# i)   Chamar a funcao max_id, guardar o que ela retorna e somar um. Esse vai ser o id do primeiro filho a ser gerado, 
-# ii)  Criar um loop for para realizar o procedimento para cada filho que precisa ser gerado.
-#   iii)  Selecionar conjuntos de pais. Chance de ser escolhido é proporcional ao fitness se maximize=True, caso contrário deve ser proporcional ao inverso do fitness.
-#   iv)   Escrever um array (numero de pais x numero de genes) com os genes dos pais. Vai servir de input para a funcao de crossover.
-#   v)    Chamar a funcao de crossover e pegar o que ela retorna
-#   vi)   Chamar a funcao de mutacao e pegar o que ela retorna
-#   vii)  Usar o np.vstack para criar um array em que cada linha corresponde a um filho e cada coluna os seus genes. A primeira coluna tem de ser o numero identificador do individuo.
-#   viii) Somar um ao identficador.
-#ix)  Escrever filhos em um novo arquivo  (id, genes)
-def tng(sorted_arr, num_new_gen, num_parents, maximize = True):
-    fitness = sorted_arr[:,-1]
-    id_new_gen = max_id() + 1
-    first = np.zeros((1,np.shape(sorted_arr)[1]-1))
-    for i in range(0,num_new_gen):
-        if (maximize == True):
-            indices = random.choices(np.arange(len(fitness)), weights = fitness, k = num_parents)
-        else:
-            indices = random.choices(np.arange(len(fitness)), weights = np.nan_to_num(1/fitness), k = num_parents)
-        parents = sorted_arr[indices,:] 
-        new_individual = crossover(parents,id_new_gen)
-        #          = mutation()
-        next_gen = np.vstack((first,new_individual))
-        id_new_gen += 1
-    next_gen = np.delete(next_gen, (0),axis = 0)
-    np.savetxt('NextGen.dat',next_gen, delimiter = '    ',fmt = '%7.4f')
-
 
 ## Crossover (Laura)
 # args: array com genes de todos os pais
@@ -155,6 +125,35 @@ def crossover(parents,id_new_gen):
 # args: array com os genes de um individuo, parametro que controla a chance de mutacao. 
 # i) Faz um loop sobre todos os genes. Decide aleatoriamente se o gene vai sofrer mutacao. Chama a funcao mutation e altera o valor do gene.
 # ii) Funcao retorna um array com os genes do filho. 
+
+## TNG (Laura)
+# args: matriz ordenada (numpy array), numero de filhos (int), numero de pais (int) (pode ser 2 ou mais), maximize (Boolean)
+# i)   Chamar a funcao max_id, guardar o que ela retorna e somar um. Esse vai ser o id do primeiro filho a ser gerado, 
+# ii)  Criar um loop for para realizar o procedimento para cada filho que precisa ser gerado.
+#   iii)  Selecionar conjuntos de pais. Chance de ser escolhido é proporcional ao fitness se maximize=True, caso contrário deve ser proporcional ao inverso do fitness.
+#   iv)   Escrever um array (numero de pais x numero de genes) com os genes dos pais. Vai servir de input para a funcao de crossover.
+#   v)    Chamar a funcao de crossover e pegar o que ela retorna
+#   vi)   Chamar a funcao de mutacao e pegar o que ela retorna
+#   vii)  Usar o np.vstack para criar um array em que cada linha corresponde a um filho e cada coluna os seus genes. A primeira coluna tem de ser o numero identificador do individuo.
+#   viii) Somar um ao identficador.
+#ix)  Escrever filhos em um novo arquivo  (id, genes)
+def tng(sorted_arr, num_new_gen, num_parents):
+    fitness = sorted_arr[:,-1]
+    id_new_gen = max_id() + 1
+    next_gen = np.zeros((1,np.shape(sorted_arr)[1]-1))
+    for i in range(0,num_new_gen):
+        if genes.maximize:
+            indices = random.choices(np.arange(len(fitness)), weights=fitness, k=num_parents)
+        else:
+            indices = random.choices(np.arange(len(fitness)), weights=np.nan_to_num(1/fitness), k=num_parents)
+        parents = sorted_arr[indices,:] 
+        new_individual = crossover(parents,id_new_gen)
+        #          = mutation()
+        next_gen = np.vstack((next_gen,new_individual))
+        id_new_gen += 1
+    next_gen = next_gen[1:,:]
+    np.savetxt('NextGen.dat',next_gen,fmt=genes.fmts, delimiter='\t')
+
 
 
 
