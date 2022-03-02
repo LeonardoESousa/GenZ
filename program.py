ab9897396103145724f4b50082dcd65bb654613d
LeonardoESousa <leonardo.sousa137@gmail.com>
2022-03-02 14:59:46 +0100

    added example for program.py

--- program.py
+++ program.py
@@ -4,3 +4,8 @@ from genz.genetic import get_genes
 
 id = sys.argv[1]
 genes = get_genes(id)
+
+resultado = abs(genes[0]**2 - genes[1]**3 + genes[2]**2)
+
+with open('Individual_'+id+'_.log', 'w') as f:
+    f.write(str(resultado))
